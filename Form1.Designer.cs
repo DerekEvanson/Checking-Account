559882ac8c5decafe4783ce4d07806c0eb2283b0
Derek Evanson <evanson.derek@gmail.com>
2018-05-17 00:48:10 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace checking_account_balance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.serviceChargeBtn = new System.Windows.Forms.RadioButton();
+            this.checkBtn = new System.Windows.Forms.RadioButton();
+            this.depositBtn = new System.Windows.Forms.RadioButton();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.summaryBtn = new System.Windows.Forms.Button();
+            this.transactionTextbox = new System.Windows.Forms.TextBox();
+            this.balanceTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.serviceChargeBtn);
+            this.groupBox1.Controls.Add(this.checkBtn);
+            this.groupBox1.Controls.Add(this.depositBtn);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // serviceChargeBtn
+            // 
+            this.serviceChargeBtn.AutoSize = true;
+            this.serviceChargeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serviceChargeBtn.Location = new System.Drawing.Point(42, 101);
+            this.serviceChargeBtn.Name = "serviceChargeBtn";
+            this.serviceChargeBtn.Size = new System.Drawing.Size(135, 24);
+            this.serviceChargeBtn.TabIndex = 0;
+            this.serviceChargeBtn.TabStop = true;
+            this.serviceChargeBtn.Text = "Service Charge";
+            this.serviceChargeBtn.UseVisualStyleBackColor = true;
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.AutoSize = true;
+            this.checkBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBtn.Location = new System.Drawing.Point(42, 68);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(72, 24);
+            this.checkBtn.TabIndex = 0;
+            this.checkBtn.TabStop = true;
+            this.checkBtn.Text = "Check";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            // 
+            // depositBtn
+            // 
+            this.depositBtn.AutoSize = true;
+            this.depositBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.depositBtn.Location = new System.Drawing.Point(42, 34);
+            this.depositBtn.Name = "depositBtn";
+            this.depositBtn.Size = new System.Drawing.Size(82, 24);
+            this.depositBtn.TabIndex = 0;
+            this.depositBtn.TabStop = true;
+            this.depositBtn.Text = "Deposit";
+            this.depositBtn.UseVisualStyleBackColor = true;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateBtn.Location = new System.Drawing.Point(12, 338);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(93, 33);
+            this.calculateBtn.TabIndex = 2;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(207, 338);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(93, 32);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(306, 338);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(90, 32);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // summaryBtn
+            // 
+            this.summaryBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.summaryBtn.Location = new System.Drawing.Point(111, 338);
+            this.summaryBtn.Name = "summaryBtn";
+            this.summaryBtn.Size = new System.Drawing.Size(90, 33);
+            this.summaryBtn.TabIndex = 3;
+            this.summaryBtn.Text = "Summary";
+            this.summaryBtn.UseVisualStyleBackColor = true;
+            this.summaryBtn.Click += new System.EventHandler(this.summaryBtn_Click);
+            // 
+            // transactionTextbox
+            // 
+            this.transactionTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.transactionTextbox.Location = new System.Drawing.Point(191, 57);
+            this.transactionTextbox.Name = "transactionTextbox";
+            this.transactionTextbox.Size = new System.Drawing.Size(205, 26);
+            this.transactionTextbox.TabIndex = 0;
+            // 
+            // balanceTextbox
+            // 
+            this.balanceTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.balanceTextbox.Location = new System.Drawing.Point(190, 104);
+            this.balanceTextbox.Name = "balanceTextbox";
+            this.balanceTextbox.Size = new System.Drawing.Size(205, 26);
+            this.balanceTextbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Transaction Amount :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "New Account Balance :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(106, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Checking Account";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 388);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.balanceTextbox);
+            this.Controls.Add(this.transactionTextbox);
+            this.Controls.Add(this.summaryBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Checking Account";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton serviceChargeBtn;
+        private System.Windows.Forms.RadioButton checkBtn;
+        private System.Windows.Forms.RadioButton depositBtn;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button summaryBtn;
+        private System.Windows.Forms.TextBox transactionTextbox;
+        private System.Windows.Forms.TextBox balanceTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* 
+ * Written by Derek Evanson
+ * With use of Coding Homework's educational material
+ * 
+ * Requirements
+ * Form: Include radio buttons to indicate the type of transaction: deposit, check, or
+ * service charge. A text box will allow the user to enter the amount of the 
+ * transaction. Display the new balance in a ReadOnly text box or a label. Calculate 
+ * the balance by adding deposits and subtracting service charges and checks. Include
+ * buttons for Calculate, Clear, and Exit. 
+ * 
+ * Add validation to the project. Display a message box if the new balance would be a 
+ * negative number. If there is not enough money to cover a check, do not deduct the
+ * check amount. Ins ad, display a message box with the message "Insufficient Funds"
+ * and deduct a servide charge of $10. 
+ * 
+ * Add a Summary button that will display the total number of deposits, the total
+ * dollar amount of deposits, the number of checks, and the dollar amount of the 
+ * checks. Do not include checks that were returned for insufficient funds, but do
+ * include the service charges. Use a message box to display the summary information 
+ */
+namespace checking_account_balance
+{
+    public partial class Form1 : Form
+    {
+        private static double balance = 0;
+        private static int totalDeposits = 0; // summary of all deposits 
+        private static double totalDepositsAmnt = 0;  // sumary of all deposit dollar amounts
+        private static int totalchecks = 0;  // sumary of all checks
+        private static double totalCheckAmnt = 0; // summary of all check amounts
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            transactionTextbox.Clear();
+            transactionTextbox.Clear();
+            depositBtn.Checked = false;
+            checkBtn.Checked = false;
+            serviceChargeBtn.Checked = false;
+            transactionTextbox.Focus();
+                
+
+        }
+        private bool validateTextBox() // return true if user enters valid number
+        {
+            double value;
+            return Double.TryParse(transactionTextbox.Text, out value);
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            char transaction = ' '; // what transaction we are performing (deposit, check, or, service charge
+            //find out what radio button is selected
+
+            if (depositBtn.Checked) { transaction = 'd'; } //deposit
+            else if (serviceChargeBtn.Checked) { transaction = 's'; } //service charge
+            else if (checkBtn.Checked) { transaction = 'c'; } //check
+
+            switch (transaction)
+            {
+                case 'd':
+                    if (validateTextBox())//validate if user entered valid number into the text box
+                    {
+                        double deposit = Convert.ToDouble(transactionTextbox.Text);
+                        depositTransaction(deposit);
+                        desplayBalance();
+                    }
+                    else
+                        errorMessage(); //else not valid
+                    break;
+                case 's':
+                    if (validateTextBox())//validate if user entered valid number into the text box
+                    {
+                        double serviceCharge = Convert.ToDouble(transactionTextbox.Text);
+                        serviceChargeTransaction(serviceCharge);
+                        desplayBalance();
+                    }
+                    else
+                        errorMessage(); //else not valid
+                    break;
+                case 'c':
+                    if (validateTextBox())//validate if user entered valid number into the text box
+                    {
+                        double check = Convert.ToDouble(transactionTextbox.Text);
+                        checkTransaction(check);
+                        desplayBalance();
+                    }
+                    else
+                        errorMessage(); //else not valid
+                    break;
+                default:
+                    MessageBox.Show("Select a transaction");
+                    break;
+            }
+        }
+
+        private void checkTransaction(double c)
+        {
+            if (balance - c >= 0)
+            {
+                balance -= c;
+                totalchecks += 1;
+                totalCheckAmnt += c;
+            }
+            else
+            {
+                balance -= 10;
+                MessageBox.Show("Insufficient funds, a $10 fee has been applied");
+                totalCheckAmnt += 10;
+            }
+        }
+
+        private void serviceChargeTransaction(double s)
+        {
+            if (balance - s >= 0) { balance -= s; }
+            else
+            {
+                MessageBox.Show("The transaction can not be complete because the balance would be less than 0");
+            }
+        }
+
+        private void errorMessage() //display error message if user dose not enter valid number into text box
+        {
+            MessageBox.Show("Invalide entery. Please enter a valid amount");
+        }
+
+        private void depositTransaction(double d) // deposit fucnction also calculates totals
+        {
+            balance += d;
+            totalDeposits += 1;
+            totalDepositsAmnt += d;
+        }
+
+        private void desplayBalance() //display balance to text box
+        {
+            balanceTextbox.Text = "$" + balance.ToString();
+        }
+
+        private void summaryBtn_Click(object sender, EventArgs e)
+        {
+            string message = "Total number of deposits:" + totalDeposits;
+            message += "\nTotal amount of deposits: $" + totalDepositsAmnt;
+            message += "\nTotal number of checks: " + totalchecks;
+            message += "\nTotal amount of checks: " + totalCheckAmnt;
+
+            MessageBox.Show(message);
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+
+}
